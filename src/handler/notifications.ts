a05340fd5011657d866b6e8c9e656864d09dcd10
Khalid <kzoabi@outlook.com>
2020-09-12 02:05:04 -0400

    Cleaning up notifications.ts and fixing ts errors

--- src/handler/notifications.ts
+++ src/handler/notifications.ts
@@ -1,24 +1,25 @@
+// tslint:disable-next-line: no-submodule-imports
 import 'source-map-support/register'
+// tslint:disable-next-line: no-implicit-dependencies
 import { DynamoDBStreamHandler } from 'aws-lambda'
 import { DynamoDB } from 'aws-sdk'
-import { handlerRouter } from '../app/environment'
-import { Resource, Event } from '../app/domain'
+import { handlerRouter } from '../lib/environment'
+import { Resource, Event } from '../lib/domain'
 import { DeployEvent, DeployCreatedEvent } from './deploys'
-import { ContentEvent, ContentCreatedEvent } from './content'
 
 export const streamHandler: DynamoDBStreamHandler = async (event, context, callback) => {
   try {
     console.log(`Request received with ${event.Records.length} event records.`)
-    for (let record of event.Records) {
+    for (const record of event.Records) {
       if (record.eventName === 'REMOVE') console.log('Event data is being deleted!')
       else if (record.eventName === 'MODIFY') console.log('Event data is being updated!')
-      else {
-        let unmarshalled = DynamoDB.Converter.unmarshall(record.dynamodb.NewImage) as Resource | Event
-        if ((unmarshalled as DeployEvent).type === 'DeployCreated') {
-          console.log(`A new deploy was created with specification: [${(unmarshalled as DeployCreatedEvent).specification}].`)
-        }
-        else if ((unmarshalled as ContentEvent).type === 'ContentCreated') {
-          console.log(`A new content was created with location: [${(unmarshalled as ContentCreatedEvent).location}].`)
+      else if (record.eventName === 'INSERT') {
+        if (record.dynamodb?.NewImage) {
+          const unmarshalled = DynamoDB.Converter.unmarshall(record.dynamodb.NewImage) as Resource | Event
+          if ((unmarshalled as DeployEvent).type === 'DeployCreated') {
+            // todo fix strange type accessor below (as as)
+            console.log(`A new deploy was created with specification: [${((unmarshalled as DeployEvent) as DeployCreatedEvent).specification}].`)
+          }
         }
       }
     }
@@ -29,7 +30,7 @@ export const streamHandler: DynamoDBStreamHandler = async (event, context, callb
   }
 }
 
-export const handler = (event, context, callback) => {
+export const handler = (event: any, context: any, callback: any) => {
   handlerRouter(event, context, callback, {
     stream: streamHandler
   })

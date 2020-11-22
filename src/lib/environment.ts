35f60662be072b005ddfb8ca186be3687c73914c
Khalid <kzoabi@outlook.com>
2020-11-22 06:48:48 -0500

    Fixing router undefined access

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -122,12 +122,13 @@ export const router = (event: any, context: any, callback: any, handlers: {
   readonly scheduled?: ScheduledHandler
   readonly topic?: SNSHandler
   readonly stream?: DynamoDBStreamHandler }) => {
-  const { httpMethod } = event as Partial<APIGatewayProxyEvent>
-  const { requestContext } = event as Partial<APIGatewayProxyEventV2>
-  if (httpMethod && handlers.api?.proxy) return handlers.api.proxy(event, context, callback)
-  else if (requestContext?.http.method && handlers.api?.proxyV2) return handlers.api.proxyV2(event, context, callback)
-  else if (handlers.api?.express && (httpMethod || requestContext?.http.method)) return serverless(handlers.api.express)(event, context)
-  else if (handlers.api?.serverless && (httpMethod || requestContext?.http.method)) return handlers.api.serverless(event, context)
+  const isProxyEvent = (event as APIGatewayProxyEvent).httpMethod
+  const isProxyV2Event = (event as APIGatewayProxyEventV2).requestContext?.http?.method
+  const isApiEvent = isProxyEvent || isProxyV2Event
+  if (isProxyEvent && handlers.api?.proxy) return handlers.api.proxy(event, context, callback)
+  else if (isProxyV2Event && handlers.api?.proxyV2) return handlers.api.proxyV2(event, context, callback)
+  else if (isApiEvent && handlers.api?.express) return serverless(handlers.api.express)(event, context)
+  else if (isApiEvent && handlers.api?.serverless) return handlers.api.serverless(event, context)
   else if ((event as SQSEvent).Records && ((event as SQSEvent).Records[0].eventSource === 'aws:sqs') && handlers.queue) return handlers.queue(event, context, callback)
   else if ((event as ScheduledEvent).source === 'aws.events' && handlers.scheduled) return handlers.scheduled(event, context, callback)
   else if ((event as SNSEvent).Records && ((event as SNSEvent).Records[0].EventSource === 'aws:sns') && handlers.topic) return handlers.topic(event, context, callback)
